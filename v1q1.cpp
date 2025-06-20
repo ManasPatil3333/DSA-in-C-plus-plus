07d1736270c570348bc59eb0ce1b17ffd01b028b
PatilManas3333 <patilmanas3333@gmail.com>
2025-06-20 08:46:59 +0530

    Initial commit

---
+++ v1q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int a, b;
+    // cin>> a >> b;
+    // int sum = a + b;
+    // cout << "sum : "<< sum<<endl;
+    int side, area;
+    cout<<"Enter side length : ";
+    cin>>side;
+    area = side*side;
+    cout<<"Area of the circle : "<<area<<endl;
+    return 0;
+}
